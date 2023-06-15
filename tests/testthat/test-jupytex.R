6da3fc4f3b29baaa1044ce76eff80fc8ab79b90d
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-15 17:42:11 +0200

    Add test for writing jupyter notebooks

--- tests/testthat/test-jupytex.R
+++ tests/testthat/test-jupytex.R
@@ -9,3 +9,11 @@ test_that("Reading jupyter notebooks works", {
     expect_equal(get_cell_source(res, 1), "Hi **Markdown**!")
     expect_equal(get_cell_source(res, 2), "print(\"Hi R Markdown!\")")
 })
+
+test_that("Writing jupyter notebooks works", {
+    nb_obj <- .read(input = toy_nb_file)
+    out_file <- tempfile(fileext = ".ipynb")
+    .write(nb_obj, out_file)
+
+    expect_true(file.exists(out_file))
+})
