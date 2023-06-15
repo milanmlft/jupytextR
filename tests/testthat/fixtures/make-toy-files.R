04282d992eb93a3ba0e57fb6f59505a7f041cd02
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-15 17:17:39 +0200

    Add toy.Rmd test fixture

--- tests/testthat/fixtures/make-toy-files.R
+++ tests/testthat/fixtures/make-toy-files.R
@@ -2,3 +2,14 @@
 nb_data <- toy_ipynb_data()
 nb_file <- test_path("fixtures", "toy.ipynb")
 jsonlite::write_json(nb_data, nb_file, auto_unbox = TRUE, pretty = TRUE)
+
+## Write Markdown test fixture
+rmd_data <- "
+Hi **Markdown**!
+
+```{r}
+print(\"Hi R Markdown!\")
+```
+"
+rmd_file <- test_path("fixtures", "toy.Rmd")
+writeLines(rmd_data, rmd_file)
