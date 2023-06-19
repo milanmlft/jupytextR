b3aee746a43ede615b9e7953b0932553df9ef33f
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-19 17:38:23 +0200

    Use different filename for the toy Rmd file

--- tests/testthat/fixtures/make-toy-files.R
+++ tests/testthat/fixtures/make-toy-files.R
@@ -4,12 +4,11 @@ nb_file <- test_path("fixtures", "toy.ipynb")
 jsonlite::write_json(nb_data, nb_file, auto_unbox = TRUE, pretty = TRUE)
 
 ## Write Markdown test fixture
-rmd_data <- "
-Hi **Markdown**!
+rmd_data <- "Hi **Markdown**!
 
 ```{r}
 print(\"Hi R Markdown!\")
 ```
 "
-rmd_file <- test_path("fixtures", "toy.Rmd")
+rmd_file <- test_path("fixtures", "toy2.Rmd")
 writeLines(rmd_data, rmd_file)
