eed4ead888b8541f49507fad69a80b76bd6759b8
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-21 16:50:33 +0100

    Don't convert Python output from jupytext
    Avoids problems with `reticulate > 1.28`, which introduced more automatic conversion of built-in Python objects.

--- R/jupytext.R
+++ R/jupytext.R
@@ -8,12 +8,12 @@ jupytext <- function(input, to, output = .with_ext(input, to), quiet = FALSE) {
 # TODO: refactor such that reticluate::import() needs to happen only once, by passing a
 # `basiliskEnvironment` to the helper functions
 .read <- function(input, ...) {
-    py_jupytext <- reticulate::import("jupytext")
+    py_jupytext <- reticulate::import("jupytext", convert = FALSE)
     py_jupytext$read(input, ...)
 }
 
 .write <- function(notebook, output) {
-    py_jupytext <- reticulate::import("jupytext")
+    py_jupytext <- reticulate::import("jupytext", convert = FALSE)
     py_jupytext$write(notebook, output)
 }
 
