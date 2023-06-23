7b25daa823d5505c628599dd61549544bf261294
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-23 14:20:46 +0100

    Don't test for the exact message in `jupytext(..., quiet = FALSE)`
    Avoiding headaches on Windows...

--- tests/testthat/test-jupytext.R
+++ tests/testthat/test-jupytext.R
@@ -1,17 +1,17 @@
 test_that("Converting from ipynb to Rmd works", {
     ipynb_file <- local_fixture("toy.ipynb")
 
-    res <- jupytext(input = ipynb_file, to = "Rmd", quiet = TRUE)
+    expect_message(
+        res <- jupytext(input = ipynb_file, to = "Rmd", quiet = FALSE)
+    )
+
     expect_true(file.exists(res))
 })
 
 test_that("Converting from R Markdown to ipynb works", {
     rmd_file <- local_fixture("toy2.Rmd")
 
-    expect_message(
-        res <- jupytext(input = rmd_file, to = "ipynb", quiet = FALSE),
-        paste("Converting", rmd_file, "to", .with_ext(rmd_file, "ipynb"))
-    )
+    res <- jupytext(input = rmd_file, to = "ipynb", quiet = TRUE)
     expect_true(file.exists(res))
 
     res <- read_ipynb(res)
