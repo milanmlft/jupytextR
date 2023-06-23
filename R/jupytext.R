0dee5cd8422807fc68316f6f1a704f57f42e53ea
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-23 15:33:01 +0100

    Add documentation

--- R/jupytext.R
+++ R/jupytext.R
@@ -1,4 +1,46 @@
+#' Convert Jupyter Notebooks to (R)Markdown, Julia, Python or R scripts and vice-versa
+#'
+#' `juptext()` provides a convenient R wrapper for the Python
+#' [`jupytext` API](https://jupytext.readthedocs.io/en/latest/using-library.html)
+#' allowing the user to convert between any of the
+#' [supported formats](https://jupytext.readthedocs.io/en/latest/formats.html) from within R.
+#'
+#' @param input File path to the input file.
+#' @param to The format to convert to, should be a file extension for any of the
+#'   [Supported Languages](https://jupytext.readthedocs.io/en/latest/languages.html). Ignored if
+#'   `output` is specified
+#' @param output The full path to where the converted file should be written. The format is derived
+#'   from the file extension of `output`. Takes precedence over `to`. By default, the output file
+#'   path is set by taking the `input` file path and replacing the file extension with `to`.
+#' @param quiet Logical, whether to show an informative message about which file is getting
+#'   converted.
+#'
+#' @return The file path to the output file, invisibly. This function is mainly called for its side
+#'   effects.
+#'
+#' @details
+#' Internally, this function calls the Python `jupytext.read` on the input file and then
+#' writes it to the desired format using `jupytext.write`, using the
+#' [`jupytext` API](https://jupytext.readthedocs.io/en/latest/using-library.html).
+#'
+#' @export
 #' @importFrom basilisk basiliskStart basiliskStop basiliskRun
+#'
+#' @examples
+#' library(jupytext)
+#'
+#' ## Create toy RMarkdown file
+#' rmd_data <- "Hi **Markdown**!
+#'
+#' ```{r}
+#' print(\"Hi R Markdown!\")
+#' ```
+#' "
+#' rmd_file <- tempfile(fileext = ".Rmd")
+#' writeLines(rmd_data, rmd_file)
+#'
+#' ## Convert Rmd file to a jupyter notebook
+#' jupytext(rmd_file, to = "ipynb")
 jupytext <- function(input, to, output = .with_ext(input, to), quiet = FALSE) {
     proc <- local_basiliskEnv(jupytext_env)
     .import_jupytext(proc)
--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,16 @@
-## Read ipynb files using jupytext's read() method
-## ipynb files are loaded in memory as an R list
+#' Read ipynb files using jupytext's read() method
+#'
+#' Calls the `jupytext.read` method from the
+#' [`jupytext` API](https://jupytext.readthedocs.io/en/latest/using-library.html).
+#' The ipynb data is loaded in memory as an R list, converted from the underlying JSON data of the
+#' notebook.
+#'
+#' @param input File path to the input file to be read.
+#'
+#' @return An R list containing the notebook data.
+#'
+#' @importFrom basilisk basiliskRun
+#' @keywords internal
 read_ipynb <- function(input) {
     proc <- local_basiliskEnv(jupytext_env)
     basiliskRun(proc, function(input) {
@@ -8,6 +19,7 @@ read_ipynb <- function(input) {
         jupytext$read(input)
     }, input = input)
 }
+
 
 # `local_` helpers for temporarily setting basilisk options -----------------------------------
 
